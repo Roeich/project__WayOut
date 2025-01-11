58c15f5c543d02afdc6c1245947e7b9718d4e06c
Roeich <roeichsheikh@gmail.com>
2025-01-11 23:58:11 +0600

    completed initial design

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
-    // управлять меню мобильной навигации
-
+    /* ........... start home page ........... */
     // Отключаем автоматическое переключение
     $('#testimonialCarousel').carousel({
         interval: false,
         wrap: true
     });
 
+    // video/image lightbox
+    const lightbox = GLightbox();
+    /* ........... end home page ........... */
+    
     /* ........... start form page ........... */
     $(".course-option .form-check-input").change(function(){
         const innerHtml=$(this).parents(".course-option").find(".course-description").html();
