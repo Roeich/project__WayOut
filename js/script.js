e21988adeb098a0d885592d8d30c610b2e52ae3b
Roeich <roeichsheikh@gmail.com>
2025-01-11 21:03:13 +0600

    working on form page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+    // управлять меню мобильной навигации
+
     // Отключаем автоматическое переключение
     $('#testimonialCarousel').carousel({
         interval: false,
         wrap: true
     });
+
+    /* ........... start form page ........... */
+    $(".course-option .form-check-input").change(function(){
+        const innerHtml=$(this).parents(".course-option").find(".course-description").html();
+        $("#course-details").html(innerHtml);
+    });
+    /* ........... end form page ........... */
 })
